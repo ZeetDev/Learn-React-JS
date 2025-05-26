607f15c3fbc8b58d18ee265fb84bf2ef137caa20
ZeetDev <zeetpatel2712@gmail.com>
2025-05-26 22:47:32 +0530

    learn about conditional rendering

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+// you can add condition directly using if
+const AgeComponent = ({
+	age,
+}: {
+	age: number;
+}) => {
+	if (age < 20) {
+		return (
+			<h1>
+				Your Age Is <strong>{age}</strong> You Are Less Then <strong>20</strong>
+			</h1>
+		);
+	}
+
+	return (
+		<h1>
+			Your Age Is <strong>{age}</strong> You Are Grater Then <strong>20</strong>
+		</h1>
+	);
+};
+
+// you can add condition directly using switch
+const ColorComponent = ({ color }: { color: string }) => {
+	switch (color) {
+		case "red":
+			return <h1>Red</h1>;
+		case "green":
+			return <h1>Green</h1>;
+		case "blue":
+			return <h1>Blue</h1>;
+		default:
+			return <h1>Unknown Color</h1>;
+	}
+};
+
+function ConditionalRendering() {
+	return (
+		<div>
+			<AgeComponent age={80} />
+			<ColorComponent color="red" />
+		</div>
+	);
+}
+
+export default ConditionalRendering;
